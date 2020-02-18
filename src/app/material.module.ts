f3c1c08853860ce1da3156c8b273b58260a30482
KelvinWanyama <kevmurumba@gmail.com>
2020-02-18 08:34:45 +0300

    Imported the material modules independently

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 
-import {
-  MatToolbarModule,MatButtonModule,
-  MatCardModule,
-  MatInputModule,
-  MatDialogModule,
-  MatTableModule,
-  MatMenuModule,
-  MatIconModule,
-  MatProgressSpinnerModule
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-} from '@angular/material';
 
 @NgModule({
   imports: [
